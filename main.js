aa13637ff9e879af004c9ceb3c09591df127b626
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-27 14:05:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// CONSEGNA
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// BONUS:
+// 1- Abbellire con CSS o Bootstrap
+// 2- Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
