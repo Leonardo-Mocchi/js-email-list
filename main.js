d03587de50279ce4795e4cdc8ef60ed2fa03f3cb
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-02-27 21:02:37 +0100

    fix formatting & add more descriptiond & add also random names

--- main.js
+++ main.js
@@ -10,25 +10,57 @@
 const randMailEl = document.querySelectorAll(".random_mail")
 console.log(randMailEl);
 
-for (let i = 0; i < 10; i++) {
+const randPerson = document.querySelectorAll(".random_person")
+console.log(randPerson);
+
+
+
+// loop for as many items as the class appears in the HTML
+for (let i = 0; i < randMailEl.length; i++) {
+
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
         .then(response => response.json())
         .then(data => {
-            //log the data to see what we have
-            console.log(data.response);
+            // log the data to see what we have
+            console.log("Random email:", data.response);
 
-            //extract the player & computer dice value with destructuring
+            // extract the mail value saving it in a variable
             const thisRandMailGenerated = data.response;
             console.log(thisRandMailGenerated);
 
-
+            // create a variable that adds a <p> HTML element inside
             const randMailNode = document.createElement("p")
-            randMailNode.innerText = thisRandMailGenerated
+            // make the new paragraph display the generated file
+            randMailNode.innerText = thisRandMailGenerated;
+            // fix the margins of the new element
+            randMailNode.classList.add('m-0');
 
             console.log(randMailNode);
 
-
-            randMailEl[i].appendChild(randMailNode)
+            // officially insert the above variable inside every i° element of the HTML
+            randMailEl[i].appendChild(randMailNode);
         })
 
+    fetch("https://flynn.boolean.careers/exercises/api/random/name")
+        .then(response => response.json())
+        .then(data => {
+            // log the data to see what we have
+            console.log("Random Person name:", data.response);
+
+            // extract the mail value saving it in a variable
+            const thisDude = data.response;
+            console.log(thisDude);
+
+            // create a variable that adds a <p> HTML element inside
+            const randDudeNode = document.createElement("p")
+            // make the new paragraph display the generated file
+            randDudeNode.innerText = thisDude;
+            // fix the margins of the new element
+            randDudeNode.classList.add('m-0');
+
+            console.log(randDudeNode);
+
+            // officially insert the above variable inside every i° element of the HTML
+            randPerson[i].appendChild(randDudeNode);
+        })
 }
